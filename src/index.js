db9cefcbc08d1a92c0435c9f0cc805aa45ab6d9b
gelizhe <2243335305@qq.com>
2023-02-12 20:27:04 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import { ChatGPTAPI } from 'chatgpt'
+
+
+  // const api = new ChatGPTAPI({
+  //   apiKey: "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"
+  // })
+
+  // const res = await api.sendMessage('Hello World!')
+  // console.log(res.text)
+console.log(111111111)
